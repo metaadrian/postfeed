7ebd59cffe629a8594560429453ff87d3501c675
metaadrian <adrian.ghita09@icloud.com>
2024-07-15 10:17:12 +0300

    Update typings.d.ts
    remove unused code

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -13,11 +13,5 @@ declare global {
             PUBLIC_URL: string;
         }
     };
-    // namespace JSX {
-    //     interface IntrinsicElements {
-    //         'cat-card': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    //         'cat-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { icon: string };
-    //     }
-    // }
 }
 
